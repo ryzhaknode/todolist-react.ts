53975d9f08f3201c77cf263a3a8f5cb468ed52ae
ryzhaknode <denryzhak@gmail.com>
2025-04-09 14:18:17 +0300

    create createTask feature

---
+++ src/components/CreateTask.tsx
@@ -0,0 +1,47 @@
+import React, {useState } from 'react';
+import '../styles/index.css';
+interface CreateTaskProps {
+    createCallback: (taskTxt: string) => void;
+}
+const CreateTask =  React.memo(({createCallback}: CreateTaskProps) => {
+    const [inputValue, setInputValue] = useState("");
+    const [isError, setIsError] = useState(false);
+
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {value} = e.target;
+        const isInvalid = /[^a-zA-Zа-яА-Я0-9 ]/.test(value);
+
+        if (!isInvalid) {
+            setInputValue(value);
+            setIsError(false);
+        } else {
+            setIsError(true);
+            setTimeout(() => {
+                setIsError(false);
+            }, 500);
+        }
+    };
+
+    const handleClickBtn = () => {
+        createCallback(inputValue);
+        setInputValue("");
+    }
+
+    return (
+        <div className="flex space-x-2 relative">
+            <input
+                value={inputValue}
+                onChange={handleInputChange}
+                className={`px-3 h-10 py-2 border rounded-md w-full sm:w-[300px] ${isError ? 'border-red-500 shake' : ''}`}
+                placeholder="Add a new task..."
+            />
+            <button disabled={inputValue === ""} onClick={handleClickBtn} className=" w-10 h-10 rounded-md font-bold p-2 bg-gray-900 text-white  hover:bg-gray-700 hover:scale-105 transition duration-300">+</button>
+            <div className={`absolute z-10 -bottom-15 sm:-bottom-10 left-0 w-[200px] sm:w-[300px] bg-red-100 text-red-700 border border-red-300 rounded-md px-3 py-2 text-sm shadow-md error-message ${isError ? 'show' : ''}`}>
+                <p>❌ Only letters and numbers are allowed.</p>
+            </div>
+        </div>
+    );
+});
+
+export default CreateTask
